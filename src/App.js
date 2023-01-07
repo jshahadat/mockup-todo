7d5987e678f02a60ff8150e57a53969bc8b5dcb4
jshahadat <jshahadat8@gmail.com>
2023-01-08 01:06:40 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './App.css';
+import Home from './components/Home/Home';
+import Update from './components/Home/Update';
+import Main from './layout/Main';
+
+function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          element: <Home></Home>
+        },
+        {
+          path: '/:id',
+          element: <Update></Update>,
+          loader: ({ params }) => fetch(`https://mockup-todos-server.vercel.app/${params.id}`)
+        }
+      ]
+    }
+  ])
+
+  return (
+    <div >
+      <RouterProvider router={router}></RouterProvider>
+
+    </div >
+  );
+}
+
+export default App;
